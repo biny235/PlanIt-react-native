40fb85614778da0aee36e7f85d9a2b5129c3b3f0
baltazarv@gmail.com <baltazar.villegas@weareheartbeat.com>
2018-05-31 20:37:16 -0400

    Added Google map.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
-import Main from './components/Root';
+import Main from './components/Main';
 
 export default class App extends Component {
   render() {
