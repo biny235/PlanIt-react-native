3e49612068be9f427316be86c314c71c8c6f3d69
sizplay@gmail.com <sizplay@gmail.com>
2018-06-07 19:31:42 -0400

    change planDetailScreen

--- screens/PlanDetailsScreen.js
+++ screens/PlanDetailsScreen.js
@@ -6,7 +6,7 @@ import DateTimePicker from 'react-native-modal-datetime-picker';
 
 export default class PlanDetailsScreen extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       isDateTimePickerVisible: false,
       time: '',
@@ -43,74 +43,71 @@ export default class PlanDetailsScreen extends Component {
     console.log(this.props.navigation);
     console.log('this.state :', this.state);
     return (
-      <Container>
-        <Content>
-          <Body style={{ marginTop: 10 }}>
-            {/*Calendar*/}
-            <Label>Select date</Label>
-            <Calendar
-              minDate={new Date()}
-              onDayPress={day => {
-                this.setState({ calendar: day.dateString })
-              }}
-              monthFormat={'yyyy MM'}
-              markedDates={{ [this.state.calendar]: { selected: true, disableTouchEvent: true, selectedDotColor: 'blue' } }}
-              firstDay={1}
+      <View style={{ marginTop: 40, backgroundColor: 'white' }}>
+        {/*Calendar*/}
+        <Label style={{ textAlign: 'center', fontWeight: 'bold' }}>SELECT DATE</Label>
+        <Calendar
+          minDate={new Date()}
+          onDayPress={day => {
+            this.setState({ calendar: day.dateString })
+          }}
+          monthFormat={'yyyy MM'}
+          markedDates={{ [this.state.calendar]: { selected: true, disableTouchEvent: true, selectedDotColor: 'blue' } }}
+          firstDay={1}
+        />
+        {/*Time*/}
+        <View style={{ margin: 10 }}>
+          <Label style={{ textAlign: 'center', fontWeight: 'bold' }}>SELECT TIME</Label>
+          <Item disabled>
+            <Input
+              style={{ alignItems: 'center' }}
+              disabled
+              placeholder={this.state.time}
             />
-            {/*Time*/}
-            <View style={{ margin: 10 }}>
-              <Label>Select time</Label>
-              <Item disabled>
-                <Input
-                  style={{ alignItems: 'center' }}
-                  disabled
-                  placeholder={this.state.time}
-                />
-                <Icon name='information-circle' />
-              </Item>
-              <Button
-                style={{ margin: 3 }}
-                primary
-                block
-                onPress={this._showDateTimePicker} >
-                <Text style={{ alignItems: 'center', color: 'white' }}>Choosing Time</Text>
-              </Button>
-              <DateTimePicker
-                isVisible={this.state.isDateTimePickerVisible}
-                onConfirm={this._handleDatePicked}
-                onCancel={this._hideDateTimePicker}
-                mode={'time'}
-                is24Hour={true}
-              />
-            </View>
-            {/*Category*/}
-            <Form>
-              <Picker
-                iosIcon={<Icon name="ios-arrow-down-outline" />}
-                placeholder="Select Category"
-                placeholderStyle={{ color: "#bfc6ea" }}
-                placeholderIconColor="#007aff"
-                style={{ width: undefined }}
-                mode='dropdown'
-                selectedValue={this.state.category}
-                onValueChange={this.onChange}
-              >
-                <Picker.Item label='Activities' value='Activities' />
-                <Picker.Item label='Movies' value='Movies' />
-                <Picker.Item label='Restaruants' value='Restaruants' />
-              </Picker>
-            </Form>
-            {/*Submit*/}
-            <Button
-              style={{ margin: 3, marginTop: 20 }}
-              primary
-              block
-              onPress={this._showDateTimePicker} >
-              <Text style={{ alignItems: 'center', color: 'white', }}>Submit</Text>
-            </Button>
-          </Body>
-        </Content>
-      </Container>
+            <Icon name='information-circle' />
+          </Item>
+          <Button
+            style={{ margin: 3 }}
+            primary
+            block
+            onPress={this._showDateTimePicker} >
+            <Text style={{ alignItems: 'center', color: 'white' }}>Choosing Time</Text>
+          </Button>
+          <DateTimePicker
+            isVisible={this.state.isDateTimePickerVisible}
+            onConfirm={this._handleDatePicked}
+            onCancel={this._hideDateTimePicker}
+            mode={'time'}
+            is24Hour={true}
+          />
+        </View>
+        {/*Category*/}
+        <Form>
+          <Picker
+            iosIcon={<Icon name="ios-arrow-down-outline" />}
+            placeholder="Select Category"
+            placeholderStyle={{ color: "#bfc6ea" }}
+            placeholderIconColor="#007aff"
+            style={{ width: 370 }}
+            mode='dropdown'
+            selectedValue={this.state.category}
+            onValueChange={this.onChange}
+          >
+            <Picker.Item label='Activities' value='Activities' />
+            <Picker.Item label='Movies' value='Movies' />
+            <Picker.Item label='Restaruants' value='Restaruants' />
+          </Picker>
+        </Form>
+        {/*Submit*/}
+        <Button
+          style={{ margin: 10, marginTop: 100, }}
+          primary
+          block
+          onPress={this._showDateTimePicker} >
+          <Text style={{ alignItems: 'center', color: 'white', }}>Submit</Text>
+        </Button>
+      </View>
     );
   }
 }
+
