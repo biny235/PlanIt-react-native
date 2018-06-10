43aacd9e738f3c714bc84d13d01877f7c1daf777
sizplay@gmail.com <sizplay@gmail.com>
2018-06-10 09:48:44 -0400

    friendplan

--- screens/GoogleSearch.js
+++ screens/GoogleSearch.js
@@ -6,8 +6,8 @@ import { connect } from 'react-redux';
 import { createPlace } from '../store/places';
 
 
-class GooglePlacesInput extends React.Component  {
-  constructor(props){
+class GooglePlacesInput extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       text: ''
@@ -16,9 +16,8 @@ class GooglePlacesInput extends React.Component  {
   }
 
 
-  onPress (data, details = null) {
-    let place;
-    if (data.place_id){
+  onPress(data, details = null) {
+    if (data.place_id) {
       place = {
         name: details.name.toString(),
         // url: details.url.toString(),
@@ -28,62 +27,67 @@ class GooglePlacesInput extends React.Component  {
         planId: 1,
         userId: this.props.users.id
       };
-    call('post', '/api/places', place);
+      call('post', '/api/places', place);
     }
   }
 
-  showContainer  = () => {
+  showContainer = () => {
     console.log('showContainer');
   }
 
   render() {
 
-  const { lat, lng, type, users } = this.props;
-  const placeHolder = type === '(cities)' ? 'Enter a City' : 'Enter a Place';
-  const { onPress } = this;
-  console.log('users', users.id);
-  return (
-    <GooglePlacesAutocomplete
-      placeholder= {placeHolder}
-      minLength={2}
-      autoFocus={false}
-      returnKeyType={'default'}
-      fetchDetails={true}
-      suppressDefaultStyles={true}
-      showContainer={this.showContainer}
-      query={{
-            key: config.GOOGLE_PLACES_KEY,
-            language: 'en',
-            location: `${lat}, ${lng}`,
-            radius: 5000,
-            types: `${type}`
-          }}
-      onPress={(data, details) => onPress(data, details)}
-      styles={{
-        listView: {
-          backgroundColor: 'blue',
-        },
-        container: {
-          flexGrow: 0.15
-        },
-        textInputContainer: {
-          backgroundColor: 'rgba(0,0,0,0)',
-          width: '100%'
-        },
-        textInput: {
-          marginLeft: 0,
-          marginRight: 0,
-          height: 38,
-          color: '#5d5d5d',
-          fontSize: 16
+    const { lat, lng, type, users } = this.props;
+    const placeHolder = type === '(cities)' ? 'Enter a City' : 'Enter a Place';
+    const { onPress } = this;
+    console.log('users', users.id);
+    return (
+      <GooglePlacesAutocomplete
+        placeholder={placeHolder}
+        minLength={2}
+        autoFocus={false}
+        returnKeyType={'default'}
+        fetchDetails={true}
+        suppressDefaultStyles={true}
+        showContainer={this.showContainer}
+        query={{
+          key: config.GOOGLE_PLACES_KEY,
+          language: 'en',
+          location: `${lat}, ${lng}`,
+          radius: 5000,
+          types: `${type}`
+        }}
+        onPress={(data, details) => {
+          console.log('data :', data);
+          console.log('details :', details);
+          onPress(data, details);
         }
-      }}
+        }
+        styles={{
+          listView: {
+            backgroundColor: 'white',
+          },
+          container: {
+            flexGrow: 1
+          },
+          textInputContainer: {
+            backgroundColor: 'rgba(0,0,0,0)',
+            width: '100%'
+          },
+          textInput: {
+            marginLeft: 0,
+            marginRight: 0,
+            height: 38,
+            color: '#5d5d5d',
+            fontSize: 16
+          }
+        }}
 
-      currentLocation={false}
-    />
-  );
+        currentLocation={false}
+      />
+    );
   }
-};
+}
 
 const mapState = ({ users }) => {
   return {
@@ -92,7 +96,7 @@ const mapState = ({ users }) => {
 };
 
 const mapDispatch = dispatch => {
-return {
+  return {
     createPlace: () => dispatch(createPlace())
   };
 };
