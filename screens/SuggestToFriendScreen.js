475d098975754364b33b3e5e5fa0f0e1d18e93e2
Alan Fleming <alalfleming@gmail.com>
2018-06-10 15:49:54 -0400

    added button, still needs route on backend

--- screens/SuggestToFriendScreen.js
+++ screens/SuggestToFriendScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Container, Content, H1 } from 'native-base';
+import { Text, View, Image } from 'react-native';
+import { Container, Content, H1, Button} from 'native-base';
 import GoogleSearch from './GoogleSearch';
 import MapView from 'react-native-maps';
 import { fetchPlaces } from '../store/places';
@@ -41,17 +41,22 @@ class SuggestToFriendScreen extends Component {
     const {  region } = this.state;
     return (
       <Container>
+      <Text style={{ height: 20 }} />
+        <Image
+          style={{alignSelf: 'center', width: 150, height: 100}}
+          source={require('../assets/PlanItLogo.png')}
+        />
         <Content padder contentContainerStyle={{ flex: 1, alignItems: 'center' }}>
           <H1 style={{ marginBottom: 10 }}>Give Moe a Suggestion</H1>
           <GoogleSearch setLoc={this.addToState} lat={chicago.lat} lng={chicago.lng} type="establishment" />
-          <Content contentContainerStyle={{ flex: 1, justifyContent: 'center' }}>
             <MapView
-              style={{ flex: 1, width: 350 }}
+              style={{ flex: 2, width: 370, marginBottom: 60 }}
               initialRegion={region}
               provider={MapView.PROVIDER_GOOGLE}
             />
-        </Content>
-          <Text style={{ flex: 1 }}>Find suggestions for your friend's plan. This screen should contain a map focused on the location where your friend is traveling to. Search for places to suggest to your friend. Your favorites should also appear on this map.</Text>
+        {this.state.googleObject && <Button block danger style={{ alignSelf: 'center', width: 200}}><Text> Add Your Suggestion </Text></Button>}
+          <Text style={{ flex: 0.5 }} />
+
         </Content>
       </Container>
     );
