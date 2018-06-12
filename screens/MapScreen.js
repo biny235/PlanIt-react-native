a8cc17a8ac8f72999f39bbefebf6a3a30c15c808
Binyamin Rosenbaum <biny235@users.noreply.github.com>
2018-06-11 20:54:02 -0400

    fix bug

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -9,35 +9,6 @@ import MapView from 'react-native-maps';
 import { fetchPlan } from '../store/plans';
 import { fetchUser } from '../store/users';
 
-const larry = require('../assets/friends-thumbnails/larry.jpg');
-const moe = require('../assets/friends-thumbnails/curly.jpg');
-const curly = require('../assets/friends-thumbnails/moe.jpg');
-const friendIcons = [moe, curly, larry];
-
-const markerData = [
-  {
-    name: 'Balthazar',
-    lat: 40.7226241814105,
-    lng: -73.99817168712616,
-    place_id: 'ChIJt7fMLIlZwokRCRtM9bNDg78',
-    details: 'Pricey but the raw red meat is great.',
-  },
-  {
-    name: "Grimaldi's Pizza",
-    lat: 40.702602314710816,
-    lng: -73.99322032928467,
-    place_id: 'ChIJgzfayTBawokR9jTsF6hLf40',
-    details: 'Homemade mozzarella cheese and beautiful view.',
-  },
-  {
-    name: 'Terri',
-    lat: 40.70661306619307,
-    lng: -74.00703504681587,
-    place_id: 'ChIJzcIh0hdawokR59-X8e5i4bk',
-    details: 'Vegan delight.',
-  },
-];
-
 const LATITUDE = 40.7050758;
 const LONGITUDE = -74.00916039999998;
 const LATITUDEDELTA = 0.0922;
@@ -62,8 +33,9 @@ class MapScreen extends Component {
 
   componentDidMount() {
     this.setState({ mapLoaded: true });
-    ! this.props.users ? this.props.fetchUser() : null;
-    ! this.props.plan ? this.props.fetchPlan() : null;
+    console.log(`Not this.props.users? ${!this.props.users}`)
+    this.props.users && !this.props.users.id ? this.props.fetchUser() : null;
+    !this.props.plan ? this.props.fetchPlan() : null;
   }
 
   onRegionChange = (region) => {
@@ -259,7 +231,7 @@ class MapScreen extends Component {
         <View style={styles.suggestPressView}>
           <Button
             style={styles.pressAreaBtn}
-            onPress={() => pnavigation.navigate('Suggestions')}
+            onPress={() => navigation.navigate('Suggestions')}
           />
         </View>
       </Container>
@@ -317,8 +289,8 @@ const styles = {
 };
 
 const mapStateToProps = ({ plans, users, friendsPlans }) => {
+  console.log(users)
   const plansCount = friendsPlans.length
-  console.log(friendsPlans)
   return {
     users,
     plans,
