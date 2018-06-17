ad8237ab2616941bc061934f92a63a9614771dc8
Binyamin Rosenbaum <biny235@users.noreply.github.com>
2018-06-16 23:04:49 -0400

    fixed duplicates when going back to the map

--- store/friendsPlans.js
+++ store/friendsPlans.js
@@ -16,7 +16,11 @@ export const fetchPlans = (id) => {
 const friendsPlansReducer = (state = [], action) => {
   switch (action.type) {
     case GET_FRIENDS_PLANS:
-      return [...state, action.plan];
+      if(state.find(plan => plan.id === action.plan.id)){
+        return state.map(plan => plan.id === action.plan.id ? action.plan : plan)
+      }else{
+        return [...state, action.plan];
+      }
     case NEW_BROADCAST:
       return state.map(plan => plan.id === action.broadcast.id ? action.broadcast : plan)
     case LOGOUT:
